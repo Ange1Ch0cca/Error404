2eb89377afae19a913606a2821c87f56d6d32930
AngelChocca <angelchoccaramos@gmail.com>
2026-02-08 17:22:17 -0500

    folder organization

---
+++ modules/learning_platform/app/views/layout/sidebar.php
@@ -0,0 +1,231 @@
+<!-- ======== sidebar-nav start =========== -->
+<aside class="sidebar-nav-wrapper">
+    <div class="navbar-logo">
+        <a href="index.html">
+            <img src="../../../assets/images/logo/logo.svg" alt="logo" />
+        </a>
+    </div>
+    <nav class="sidebar-nav">
+        <ul>
+            <li class="nav-item">
+                <a href="panel.php">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M12,1L3,5V11C3,16.55 6.84,21.74 12,23C17.16,21.74 21,16.55 21,11V5L12,1M12,7A3,3 0 0,1 15,10A3,3 0 0,1 12,13A3,3 0 0,1 9,10A3,3 0 0,1 12,7M12,14.5C14.33,14.5 18,15.67 18,18V19H6V18C6,15.67 9.67,14.5 12,14.5Z" />
+                        </svg>
+                    </span>
+                    <span class="text">Administrador</span>
+                </a>
+            </li>
+            <span class="divider">
+                <hr />
+            </span>
+
+            <li class="nav-item">
+                <a href="inicio.php">
+                    <span class="icon">
+                        <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M16 8C16 10.21 14.21 12 12 12C9.79 12 8 10.21 8 8L8.11 7.06L5 5.5L12 2L19 5.5V10.5H18V6L15.89 7.06L16 8M12 14C16.42 14 20 15.79 20 18V20H4V18C4 15.79 7.58 14 12 14Z" />
+                        </svg>
+                    </span>
+                    <span class="text">Inicio</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="materiales.php">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M15 8C12.79 8 11 9.79 11 12V20H4C2.9 20 2 19.11 2 18V6C2 4.89 2.89 4 4 4H10L12 6H20C21.1 6 22 6.89 22 8V10.17L20.41 8.59L19.83 8H15M23 14V21C23 22.11 22.11 23 21 23H15C13.9 23 13 22.11 13 21V12C13 10.9 13.9 10 15 10H19L23 14M21 14.83L18.17 12H18V15H21V14.83Z" />
+                        </svg>
+                    </span>
+
+                    <span class="text">Materiales</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="actividades.php">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M17,4V10L15,8L13,10V4H9V20H19V4H17M3,7V5H5V4C5,2.89 5.9,2 7,2H19C20.1,2 21,2.9 21,4V20C21,21.1 20.1,22 19,22H7C5.9,22 5,21.1 5,20V19H3V17H5V15H3V13H5V11H3V9H5V7H3M7,4V20H19V4H7Z" />
+                        </svg>
+                    </span>
+
+
+                    <span class="text">Actividades</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="calificaciones.php">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M13,2L3,2V22H21V10L13,2M13,3.5L18.5,9H13V3.5M10.8,11.3L13.1,19H11L10.5,17.4H7.8L7.3,19H5.2L7.5,11.3H10.8M8.2,15.8H10.1L9.1,12.7L8.2,15.8M15,13V15H13V13H15M17,13V15H15V13H17M15,11V13H13V11H15M17,11V13H15V11H17Z" />
+                        </svg>
+                    </span>
+
+
+                    <span class="text">Calificaciones</span>
+                </a>
+            </li>
+            <span class="divider">
+                <hr />
+            </span>
+
+            <li class="nav-item">
+                <a href="panel.php">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                            <!-- Pizarra de fondo -->
+                            <path d="M20,17H22V5H2V17H11.5V19H8V21H16V19H12.5V17H20M3,6H21V16H3V6Z" opacity="0.3" />
+                            <!-- Silueta neutra con puntero/libro -->
+                            <path d="M9,5A4,4 0 0,1 13,9A4,4 0 0,1 9,13A4,4 0 0,1 5,9A4,4 0 0,1 9,5M9,15C11.67,15 17,16.34 17,19V21H1V19C1,16.34 6.33,15 9,15Z" />
+                        </svg>
+                    </span>
+                    <span class="text">Panel</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="materiales.php">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M15 8C12.79 8 11 9.79 11 12V20H4C2.9 20 2 19.11 2 18V6C2 4.89 2.89 4 4 4H10L12 6H20C21.1 6 22 6.89 22 8V10.17L20.41 8.59L19.83 8H15M23 14V21C23 22.11 22.11 23 21 23H15C13.9 23 13 22.11 13 21V12C13 10.9 13.9 10 15 10H19L23 14M21 14.83L18.17 12H18V15H21V14.83Z" />
+                        </svg>
+                    </span>
+
+                    <span class="text">Add Materiales</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="actividades.php">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M17,4V10L15,8L13,10V4H9V20H19V4H17M3,7V5H5V4C5,2.89 5.9,2 7,2H19C20.1,2 21,2.9 21,4V20C21,21.1 20.1,22 19,22H7C5.9,22 5,21.1 5,20V19H3V17H5V15H3V13H5V11H3V9H5V7H3M7,4V20H19V4H7Z" />
+                        </svg>
+                    </span>
+
+
+                    <span class="text">Add Actividades</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="calificaciones.php">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M13,2L3,2V22H21V10L13,2M13,3.5L18.5,9H13V3.5M10.8,11.3L13.1,19H11L10.5,17.4H7.8L7.3,19H5.2L7.5,11.3H10.8M8.2,15.8H10.1L9.1,12.7L8.2,15.8M15,13V15H13V13H15M17,13V15H15V13H17M15,11V13H13V11H15M17,11V13H15V11H17Z" />
+                        </svg>
+                    </span>
+
+
+                    <span class="text">Add Calificaciones</span>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a href="calificaciones.php">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M19,19H5V8H19M16,1V3H8V1H6V3H5C3.89,3 3,3.89 3,5V19A2,2 0 0 0 5,21H19A2,2 0 0 0 21,19V5C21,3.89 20.1,3 19,3H18V1M17,12H12V17H17V12Z" />
+                        </svg>
+                    </span>
+
+
+                    <span class="text">Add Asistencia</span>
+                </a>
+            </li>
+
+            <span class="divider">
+                <hr />
+            </span>
+
+            <li class="nav-item">
+                <a href="calificaciones.php">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M13,9H18.5L13,3.5V9M6,2H14L20,8V20A2,2 0 0 1 18,22H6C4.89,22 4,21.1 4,20V4C4,2.89 4.89,2 6,2M7,12V14H17V12H7M7,16V18H17V16H7M7,8V10H11V8H7Z" />
+                        </svg>
+                    </span>
+
+
+                    <span class="text">Registro Auxiliar</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a href="calificaciones.php">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M19,19H5V8H19M16,1V3H8V1H6V3H5C3.89,3 3,3.89 3,5V19A2,2 0 0 0 5,21H19A2,2 0 0 0 21,19V5C21,3.89 20.1,3 19,3H18V1M17,12H12V17H17V12Z" />
+                        </svg>
+                    </span>
+
+
+                    <span class="text">Registro Asistencia</span>
+                </a>
+            </li>
+
+            <span class="divider">
+                <hr />
+            </span>
+
+            <li class="nav-item nav-item-has-children">
+                <a
+                    href="#0"
+                    class="collapsed"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#ddmenu_2"
+                    aria-controls="ddmenu_2"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span class="icon">
+                        <svg xmlns="http://www.w3.org" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+                            <path d="M21 7H3V3H21V7M9.5 11H14.5C14.78 11 15 11.22 15 11.5V12.06C15.87 10.83 17.35 10 19 10C19.34 10 19.68 10.04 20 10.11V8H4V21H13.03C13 20.9 13 20.8 13 20.7V17.2C13 16.24 13.5 15.34 14.2 14.74V14.5C14.2 14 14.32 13.47 14.5 13H9V11.5C9 11.22 9.22 11 9.5 11M23 17.3V20.8C23 21.4 22.4 22 21.7 22H16.2C15.6 22 15 21.4 15 20.7V17.2C15 16.6 15.6 16 16.2 16V14.5C16.2 13.1 17.6 12 19 12S21.8 13.1 21.8 14.5V16C22.4 16 23 16.6 23 17.3M20.5 14.5C20.5 13.7 19.8 13.2 19 13.2S17.5 13.7 17.5 14.5V16H20.5V14.5Z" />
+                        </svg>
+                    </span>
+                    <span class="text">Otros</span>
+                </a>
+                <ul id="ddmenu_2" class="collapse dropdown-nav">
+                    <li>
+                        <a href="#"> Formatos </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <!--Caja de Actualiza-->
+    <div class="promo-box">
+        <div class="promo-icon">
+            <img class="mx-auto" src="./../../../assets/images/logo/logo-icon-big.svg" alt="Logo">
+        </div>
+        <h3>Actualizar a PRO</h3>
+        <p>¡Mejore su proceso de desarrollo y comience a hacer más con PlainAdmin PRO!</p>
+        <a href="https://plainadmin.com/pro" target="_blank" rel="nofollow" class="main-btn primary-btn btn-hover">
+            Actualizar a PRO
+        </a>
+    </div>
+</aside>
+<div class="overlay"></div>
+<!-- ======== sidebar-nav end =========== -->
+
+<script>
+    const currentPage = window.location.pathname.split("/").pop();
+
+    document.querySelectorAll(".sidebar-nav a").forEach(link => {
+        const parentItem = link.closest(".nav-item");
+
+        if (link.getAttribute("href") === currentPage) {
+            parentItem?.classList.add("active");
+
+            // Si está dentro de un submenú
+            const collapseMenu = link.closest(".collapse");
+            if (collapseMenu) {
+                collapseMenu.classList.add("show");
+                collapseMenu
+                    .closest(".nav-item")
+                    .querySelector("a[data-bs-toggle='collapse']")
+                    ?.classList.remove("collapsed");
+            }
+        } else {
+            parentItem?.classList.remove("active");
+        }
+    });
+</script>
